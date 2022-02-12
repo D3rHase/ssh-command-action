c6b919f4bb5df0cd8fa42b1cd36fb86480a94917
D3rHase <48031167+D3rHase@users.noreply.github.com>
2022-02-12 14:37:01 +0100

    v0.1

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM alpine:3.15
+
+RUN apk add --no-cache openssh bash
+
+# Copies the entrypoint.sh file from the repository to the filesystem path `/` of the container
+COPY entrypoint.sh /entrypoint.sh
+
+# Make the bash script executable
+RUN chmod +x /entrypoint.sh
+
+# Code file to execute when the docker container starts up (`entrypoint.sh`)
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Prepare SSH connection
+mkdir -p /root/.ssh/
+echo "${INPUT_PRIVATE_SSH_KEY}" > /root/.ssh/id_rsa.key
+chmod 600 /root/.ssh/id_rsa.key
+ssh-keyscan -H ${INPUT_HOST} >> /root/.ssh/known_hosts
+cat >>/root/.ssh/config <<END
+Host server
+    HostName ${INPUT_HOST}
+    User ${INPUT_USER}
+    Port ${INPUT_PORT}
+    IdentityFile /root/.ssh/id_rsa.key
+END
+
+# Execute the command
+ssh server $INPUT_COMMAND
